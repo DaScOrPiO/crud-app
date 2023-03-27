72773ef7c25e2b19e4289889e3a33a4ab6fdcdba
DaScOrPiO <dunni82@yahoo.com>
2023-03-27 23:44:21 +0100

    Fix: minor bug fix

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -7,119 +7,116 @@ import { CgDisplayGrid } from "react-icons/cg";
 import { BiCalendar } from "react-icons/bi";
 import Button from "@/components/button";
 
-
-export default function Dashboard() {
+export default function Dashboard({ fixedHeight }) {
   return (
-    <div className="sidebar lg:w-1/4 md:w-2/4 w-full md:h-auto sm:h-auto bg-yellow-400 flex flex-col px-4">
-    <div className="logo-con w-full h-auto p-2">
-      <h2 className="text-2xl mt-4 font-bold">Taskmate</h2>
-      <p>Focus, priortize, Excecute</p>
+    <div
+      className="sidebar lg:w-1/4 w-full bg-yellow-400 flex flex-col px-4"
+      // style={{ height: fixedHeight }}
+    >
+      <div className="logo-con w-full p-2" style={{height: `calc(100% - ${fixedHeight})`}}>
+        <h2 className="text-2xl mt-4 font-bold">Taskmate</h2>
+        <p>Focus, priortize, Excecute</p>
 
-      <div className="bg-gray-100 rounded-lg w-full mt-4 flex">
-        <div className="rounded-full w-1/4 flex items-center justify-center">
-          <Image
-            className="w-2/4 rounded-full h-3/4"
-            src={require("../images/demo-img.jpg")}
-            alt=""
-            width={100}
-          />
-        </div>
+        <div className="bg-gray-100 rounded-lg w-full mt-4 flex">
+          <div className="rounded-full w-1/4 flex items-center justify-center">
+            <Image
+              className="w-2/4 rounded-full h-3/4"
+              src={require("../images/demo-img.jpg")}
+              alt=""
+              width={100}
+            />
+          </div>
 
-        <div className="w-3/5 flex flex-col items-center justify-center">
-          <h2>Irvan Moses</h2>
-          <h2>Example@yahoo.com</h2>
-        </div>
-      </div>
-    </div>
-
-    <div className="greetings h-auto p-2 w-full mt-4">
-      <h2 className="text-center text-2xl">Good morning Oladunni</h2>
-
-      <div className="flex flex-col mt-6">
-        <div className="flex justify-between mt-4">
-          <h2 className="text-2xl">Menu</h2>
-          <div className="text-2xl hover:text-gray-300 hover:cursor-pointer">
-            <MdOutlineExpandMore />
+          <div className="w-3/5 flex flex-col items-center justify-center">
+            <h2>Irvan Moses</h2>
+            <h2>Example@yahoo.com</h2>
           </div>
         </div>
+      </div>
 
+      <div className="greetings h-auto p-2 w-full mt-4">
+        <h2 className="text-center text-2xl">Good morning Oladunni</h2>
+
+        <div className="flex flex-col mt-6">
+          <div className="flex justify-between mt-4">
+            <h2 className="text-2xl">Menu</h2>
+            <div className="text-2xl hover:text-gray-300 hover:cursor-pointer">
+              <MdOutlineExpandMore />
+            </div>
+          </div>
+
+          <nav>
+            <ul className="nav-container">
+              <li className="list">
+                <span className="mr-4">
+                  <GrOverview />
+                </span>
+                <Link className="link" href="">
+                  Overview
+                </Link>
+              </li>
+
+              <li className="list">
+                <span className="mr-4">
+                  <BsReverseListColumnsReverse />
+                </span>
+                <Link className="link flex" href="">
+                  Task List
+                </Link>
+              </li>
+
+              <li className="list">
+                <span className="mr-4">
+                  <CgDisplayGrid />
+                </span>
+                <Link className="link flex" href="">
+                  Project Overview
+                </Link>
+              </li>
+
+              <li className="list">
+                <span className="mr-4">
+                  <BiCalendar />
+                </span>
+                <Link className="link flex" href="">
+                  Calendar
+                </Link>
+              </li>
+
+              <li className="list">
+                <span className="mr-4">
+                  <GrUserSettings />
+                </span>
+                <Link className="link flex" href="">
+                  Settings
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="flex justify-center mt-12 flex-col">
         <nav>
           <ul className="nav-container">
             <li className="list">
-              <span className="mr-4">
-                <GrOverview />
-              </span>
-              <Link className="link" href="">
-                Overview
+              <span className="mr-4">😎</span>
+              <Link className="link" id="thin-font" href="">
+                personal
               </Link>
             </li>
 
             <li className="list">
-              <span className="mr-4">
-                <BsReverseListColumnsReverse />
-              </span>
-              <Link className="link flex" href="">
-                Task List
-              </Link>
-            </li>
-
-            <li className="list">
-              <span className="mr-4">
-                <CgDisplayGrid />
-              </span>
-              <Link className="link flex" href="">
-                Project Overview
-              </Link>
-            </li>
-
-            <li className="list">
-              <span className="mr-4">
-                <BiCalendar />
-              </span>
-              <Link className="link flex" href="">
-                Calendar
-              </Link>
-            </li>
-
-            <li className="list">
-              <span className="mr-4">
-                <GrUserSettings />
-              </span>
-              <Link className="link flex" href="">
-                Settings
+              <span className="mr-4">💻</span>
+              <Link className="link flex" id="thin-font" href="">
+                Work/Studies
               </Link>
             </li>
           </ul>
         </nav>
+
+        <Button text="Add task" />
       </div>
     </div>
-
-    <div className="flex justify-center mt-12 flex-col">
-      <nav>
-        <ul className="nav-container">
-          <li className="list">
-            <span className="mr-4">
-              😎
-            </span>
-            <Link className="link" id="thin-font" href="">
-              personal
-            </Link>
-          </li>
-
-          <li className="list">
-            <span className="mr-4">
-              💻
-            </span>
-            <Link className="link flex" id="thin-font" href="">
-              Work/Studies
-            </Link>
-          </li>
-        </ul>
-      </nav>
-
-      <Button text="Add task" />
-    </div>
-  </div>
-
-  )
+  );
 }
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -1,12 +1,23 @@
 import Dashboard from "@/components/dashboard";
 import Task from "@/components/Task";
+// import { useState, useLayoutEffect, useRef } from "react";
 
 export default function Homepage() {
+  // const [Height, setHeight] = useState(0);
+  // const ref = useRef(null);
+
+  // useLayoutEffect(() => {
+  //   setHeight(ref.current.offsetHeight);
+  // }, []);
+
   return (
-    <div className="bg-blue-900 w-screen h-auto flex md:flex-row flex-col">
+    <div className="page-container bg-blue-900 w-screen flex md:flex-row flex-col">
       <Dashboard />
 
-      <div className="md:w-3/4 w-full bg-red-400 flex flex-col px-4 py-4">
+      <div
+        className="page-items bg-red-400 flex flex-col px-4 py-4"
+        // style={{ height: Height }}
+      >
         <header
           className="header flex justify-between
          text-white w-full p-2 bg-orange-500 items-end"
@@ -15,7 +26,10 @@ export default function Homepage() {
           <h2 className="text-center text-2xl">20/03/2023</h2>
         </header>
 
-        <div className="w-full mt-4 bg-purple-400 flex flex-col py-4">
+        <div
+          className="w-full mt-4 bg-purple-400 flex flex-col py-4"
+          // style={{ height: `calc(100% - ${Height})` }}
+        >
           <h2 className="text-2xl font-bold mt-6">Tasks</h2>
 
           <div className="flex flex-col mt-4">
